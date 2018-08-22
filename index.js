962e6d6e0d3e1a8d9da24e4f9b56a0f219c466cc
Nitoblock <35974243+Nitoblock@users.noreply.github.com>
2018-08-22 22:25:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bot = new Discord.Client();
 
 bot.on("ready", () => {
     bot.user.setActivity("preparation du serveur",{type: "PLAYING"});
+    console.log("[Plugline] Je suis actif");
 });
 
 bot.login(process.env.BOT_TOKEN);
