79bebbcf07f0d409bedce5a6220708393549aec0
Nitoblock <35974243+Nitoblock@users.noreply.github.com>
2018-08-22 23:16:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,26 +8,4 @@ bot.on("ready", () => {
 });
 
 
-bot.on("message", message => {
-    // Partie commande
-    var command = message.split(" ")[0].toLowerCase();
-    var args = message.split(" ").shift();
-    
-    if (command == "/help") {
-        var embed = new Discord.RichEmbed();
-        embed.setColor(0xFFBB00);
-        embed.setTitle("Liste des commandes");
-        embed.addField("/help", "Permet d’avoir la liste des commandes");
-        embed.addField("/ping", "Permet de récupérer le ping du bot");
-           if (message.author.member.hasPermission("ADMINISTRATOR")) embed.addFied("/annonde [texte]", "Permet de faire une annonce sur le serveur");
-        
-        message.channel.send({embed}).then(msg => {msg.delete(10000)});
-        message.delete();
-    }
-
-
-    // Partie message
-});
-
-
 bot.login(process.env.BOT_TOKEN);
