1d84a4f4576b1936e774ec0218133f064b02301b
Alex77750 <42590776+Alex77750@users.noreply.github.com>
2018-08-22 14:43:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client()
 bot.on('ready', () => {
     bot.user.setActivity('preparation du serveur',{type: "PLAYING"}).catch(console.error)
 
-client.on('message', async msg => {
+bot.on('message', async msg => {
   if (msg.content === 'test') {
     const channel = await message.guild.channels.create('foo');
     await channel.setParent('481207199939559425'); 
