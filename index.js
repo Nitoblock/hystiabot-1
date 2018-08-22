c318c85fc1c9652772a8b844cb411ea104d4d54d
Alex77750 <42590776+Alex77750@users.noreply.github.com>
2018-08-22 14:26:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
+bot.on('ready', () => {
+    bot.user.setActivity('preparation du serveur',{type: "PLAYING"}).catch(console.error)
+
+
+
+
+
+
+});
+
+
 
 bot.login(process.env.BOT_TOKEN);
