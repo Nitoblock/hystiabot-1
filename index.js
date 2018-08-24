0104a5a43ee6b37f6dc9848debde93dd2b83d1af
Alex77750 <42590776+Alex77750@users.noreply.github.com>
2018-08-24 02:23:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,22 @@ bot.on("ready", () => {
     console.log("[Plugline] Je suis actif (pour une fois)");
 });
 
+bot.on("message", message => {
+	// Commandes
+	var command = message.content.split(" ")[0].toLowerCase();
+	var args = message.content.split(" ").slice(1);
+
+	if (command == "/say") {
+		if (message.member.hasPermission("ADMINISTRATOR")) {
+			if (args.length == 0) {
+				message.channel.send(message.author + ", utilise /say <message>").then(msg => {msg.delete(10000)});
+			} else {
+				message.channel.send(args.join(" "));
+			}
+		}
+
+		message.delete();
+	}
+
 
 bot.login(process.env.BOT_TOKEN);
